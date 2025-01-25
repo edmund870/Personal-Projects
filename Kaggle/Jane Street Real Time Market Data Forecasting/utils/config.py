08a143b603cce6895b7d459b5950f68c6bce5b2b
Edmund Chia <edmund870@gmail.com>
2025-01-25 15:16:13 +0800

    final

--- Kaggle/Jane Street Real Time Market Data Forecasting/utils/config.py
+++ Kaggle/Jane Street Real Time Market Data Forecasting/utils/config.py
@@ -7,17 +7,14 @@ class CONFIG:
 
     # Define the feature names based on the number of features (79 in this case)
     feature_names = [f"feature_{i:02d}" for i in range(79)]
+    categorical_feature = [f"feature_{i:02d}" for i in range(9,12)]
+    numerical_feature = [f"feature_{i:02d}" for i in range(79) if i not in range(9,12)]
     exogeneous_features = [
         "sin_time_id",
         "cos_time_id",
         "sin_time_id_halfday",
         "cos_time_id_halfday",
     ]
-    lag_features = [f"responder_{idx}_lag_1" for idx in range(9)] + [
-        f"responder_{idx}_lag_1_{stat}_{i}"
-        for idx in range(9)
-        for i in range(2, 6)
-        for stat in ["mean", "std", "skew"]
-    ]
+    lag_features = [f"responder_{idx}_lag_1" for idx in range(9)]
 
     N_fold = 5
